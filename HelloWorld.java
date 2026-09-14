d4b3ade5a13eeac195b615260b4986430afe1cb0
Omar <omar.alghamdi770@gmail.com>
2026-09-14 13:50:22 +0300

    Omar

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
 public class HelloWorld {
 public static void main(String[] args) {
 System.out.println("Hello, SE4111!");
-System.out.println("Welcome to Software Construction");
-System.out.println("activity 5");}
-
+System.out.println("Welcome to Software Construction Mr Omar");
+System.out.println("activity 5");
+System.out.println("Mr Omar");
+}
 }
