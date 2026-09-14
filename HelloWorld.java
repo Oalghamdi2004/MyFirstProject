38f00800b37665e6c81f899c3a8cd447aa3d12d0
Omar <omar.alghamdi770@gmail.com>
2026-09-14 12:58:28 +0300

    lab2

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
 public static void main(String[] args) {
 System.out.println("Hello, SE4111!");
 System.out.println("Welcome to Software Construction");
-}
+System.out.println("activity 5");}
+
 }
